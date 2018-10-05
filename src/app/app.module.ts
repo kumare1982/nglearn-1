4907e9c649d6007159771a79b9ee1df96156dbca
GENBLR\kumaresank <kumaresan.k@genisys-group.com>
2018-10-05 19:00:27 +0530

    angular learning continues...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { ArtistItemsComponent } from './artist-items/artist-items.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistItemsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
